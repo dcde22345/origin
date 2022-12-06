d952b2e15d661c8ad2ea34ba437e3910c211901c
dcde22345 <dcde22345@gmail.com>
2022-12-06 14:11:56 +0800

    additionl exesice

---
+++ 額外練習/第二題/Fibonacci_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int fibo(int n1, int n2,int n3, int n4, int n, int t){
+    if(n == t+1){
+        printf("%d", n1 + n2);
+        return 0;
+    }
+
+    return fibo(, n1+n2, n+1, t);
+}
+int main(void){
+    int n1, n2, n3 , n4, t;
+    scanf("%d %d %d %d %d", &n1, &n2, &n3, &n4, &t);
+
+    fibo(n1, n2, n3, n4, 2, t);
+    return 0;
+}
